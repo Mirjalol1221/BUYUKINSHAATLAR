40559804fb905db23f3812dd59c93a271fe470b2
Mirjalol1221 <150045556+Mirjalol1221@users.noreply.github.com>
2024-01-02 21:29:24 +0500

    about.html

---
+++ About.vue
@@ -0,0 +1,366 @@
+<template>
+  <div class="about">
+    <div class="container">
+      <div class="left">
+        <img
+          src="../assets/images/about-img.jpg"
+          alt=""
+          data-aos="fade-right"
+          data-aos-offset="250"
+          data-aos-duration="1500"
+        />
+        />
+      </div>
+      <div class="right">
+        <div class="link-about">
+          <span
+            ><svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="27"
+              width="35"
+              viewBox="0 0 512 512"
+              data-aos="fade-left"
+              data-aos-offset="250"
+              data-aos-duration="1500"
+            >
+              <path
+                fill="#03adfc"
+                d="M502.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-128-128c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L402.7 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l370.7 0-73.4 73.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l128-128z"
+              /></svg
+          ></span>
+          <h4
+            data-aos="fade-left"
+            data-aos-offset="250"
+            data-aos-duration="1500"
+          >
+            KOMPANIYA HAQIDA
+          </h4>
+        </div>
+
+        <h1 data-aos="fade-left" data-aos-offset="250" data-aos-duration="1500">
+          Высокое <br />
+          качество обслуживания с 2000 года
+        </h1>
+        <p data-aos="fade-left" data-aos-offset="200" data-aos-duration="1500">
+          Располагая крупнейшим в Средней Азии автоматизированным
+          производственным комплексом, компания AKFA предлагает только
+          современные решения, качество и надежность которых проверены временем.
+        </p>
+      </div>
+    </div>
+  </div>
+  <div class="about-2">
+    <div class="container">
+      <div class="wrapper">
+        <div
+          class="card"
+          data-aos="flip-left"
+          data-aos-offset="250"
+          data-aos-duration="1500"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="46"
+            width="50"
+            viewBox="0 0 576 512"
+          >
+            <!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2023 Fonticons, Inc.-->
+            <path
+              fill="#03adfc"
+              d="M408 120c0 54.6-73.1 151.9-105.2 192c-7.7 9.6-22 9.6-29.6 0C241.1 271.9 168 174.6 168 120C168 53.7 221.7 0 288 0s120 53.7 120 120zm8 80.4c3.5-6.9 6.7-13.8 9.6-20.6c.5-1.2 1-2.5 1.5-3.7l116-46.4C558.9 123.4 576 135 576 152V422.8c0 9.8-6 18.6-15.1 22.3L416 503V200.4zM137.6 138.3c2.4 14.1 7.2 28.3 12.8 41.5c2.9 6.8 6.1 13.7 9.6 20.6V451.8L32.9 502.7C17.1 509 0 497.4 0 480.4V209.6c0-9.8 6-18.6 15.1-22.3l122.6-49zM327.8 332c13.9-17.4 35.7-45.7 56.2-77V504.3L192 449.4V255c20.5 31.3 42.3 59.6 56.2 77c20.5 25.6 59.1 25.6 79.6 0zM288 152a40 40 0 1 0 0-80 40 40 0 1 0 0 80z"
+            />
+          </svg>
+          <h1>284</h1>
+          <p>Тыс.квм Площадь</p>
+          <button>ПОДРОБНЕЕ</button>
+        </div>
+        <div
+          class="card"
+          data-aos="flip-left"
+          data-aos-offset="250"
+          data-aos-duration="1500"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="46"
+            width="50"
+            viewBox="0 0 640 512"
+          >
+            <!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2023 Fonticons, Inc.-->
+            <path
+              fill="#03adfc"
+              d="M200 0c-30.6 0-56 54.7-56 86.1V192.5L7.8 274.3C2.9 277.2 0 282.4 0 288v64c0 5.1 2.4 9.8 6.4 12.8s9.3 3.9 14.1 2.5l123.4-37v81.2l-50 40c-3.8 3-6 7.6-6 12.5v32c0 5.1 2.5 10 6.6 13s9.5 3.8 14.4 2.2L200 480.9 290.4 511c-1.6-4.7-2.4-9.8-2.4-15V463.4c-18.2-10.5-30.7-29.7-31.9-51.8l-.1-.1V408 325.5 184l0-1.1 0 0V86.1C256 54.7 231.5 0 200 0zm88 176V400c0 20.9 13.4 38.7 32 45.3V488c0 13.3 10.7 24 24 24h16c13.3 0 24-10.7 24-24V448H544v40c0 13.3 10.7 24 24 24h16c13.3 0 24-10.7 24-24V445.3c18.6-6.6 32-24.4 32-45.3V176c0-26.5-21.5-48-48-48H336c-26.5 0-48 21.5-48 48zm79.8 78.7c3.3-8.7 11.2-14.7 20.5-14.7H539.7c9.2 0 17.2 6 20.5 14.7L576 304H352l15.8-49.3zM568 352a24 24 0 1 1 0 48 24 24 0 1 1 0-48zM336 376a24 24 0 1 1 48 0 24 24 0 1 1 -48 0z"
+            />
+          </svg>
+          <h1>14</h1>
+          <p>Страна Экспорта</p>
+          <button>ПОДРОБНЕЕ</button>
+        </div>
+        <div
+          class="card"
+          data-aos="flip-left"
+          data-aos-offset="250"
+          data-aos-duration="1500"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="46"
+            width="54"
+            viewBox="0 0 448 512"
+          >
+            <!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2023 Fonticons, Inc.-->
+            <path
+              fill="#03adfc"
+              d="M86.4 5.5L61.8 47.6C58 54.1 56 61.6 56 69.2V72c0 22.1 17.9 40 40 40s40-17.9 40-40V69.2c0-7.6-2-15-5.8-21.6L105.6 5.5C103.6 2.1 100 0 96 0s-7.6 2.1-9.6 5.5zm128 0L189.8 47.6c-3.8 6.5-5.8 14-5.8 21.6V72c0 22.1 17.9 40 40 40s40-17.9 40-40V69.2c0-7.6-2-15-5.8-21.6L233.6 5.5C231.6 2.1 228 0 224 0s-7.6 2.1-9.6 5.5zM317.8 47.6c-3.8 6.5-5.8 14-5.8 21.6V72c0 22.1 17.9 40 40 40s40-17.9 40-40V69.2c0-7.6-2-15-5.8-21.6L361.6 5.5C359.6 2.1 356 0 352 0s-7.6 2.1-9.6 5.5L317.8 47.6zM128 176c0-17.7-14.3-32-32-32s-32 14.3-32 32v48c-35.3 0-64 28.7-64 64v71c8.3 5.2 18.1 9 28.8 9c13.5 0 27.2-6.1 38.4-13.4c5.4-3.5 9.9-7.1 13-9.7c1.5-1.3 2.7-2.4 3.5-3.1c.4-.4 .7-.6 .8-.8l.1-.1 0 0 0 0s0 0 0 0s0 0 0 0c3.1-3.2 7.4-4.9 11.9-4.8s8.6 2.1 11.6 5.4l0 0 0 0 .1 .1c.1 .1 .4 .4 .7 .7c.7 .7 1.7 1.7 3.1 3c2.8 2.6 6.8 6.1 11.8 9.5c10.2 7.1 23 13.1 36.3 13.1s26.1-6 36.3-13.1c5-3.5 9-6.9 11.8-9.5c1.4-1.3 2.4-2.3 3.1-3c.3-.3 .6-.6 .7-.7l.1-.1c3-3.5 7.4-5.4 12-5.4s9 2 12 5.4l.1 .1c.1 .1 .4 .4 .7 .7c.7 .7 1.7 1.7 3.1 3c2.8 2.6 6.8 6.1 11.8 9.5c10.2 7.1 23 13.1 36.3 13.1s26.1-6 36.3-13.1c5-3.5 9-6.9 11.8-9.5c1.4-1.3 2.4-2.3 3.1-3c.3-.3 .6-.6 .7-.7l.1-.1c2.9-3.4 7.1-5.3 11.6-5.4s8.7 1.6 11.9 4.8l0 0 0 0 0 0 .1 .1c.2 .2 .4 .4 .8 .8c.8 .7 1.9 1.8 3.5 3.1c3.1 2.6 7.5 6.2 13 9.7c11.2 7.3 24.9 13.4 38.4 13.4c10.7 0 20.5-3.9 28.8-9V288c0-35.3-28.7-64-64-64V176c0-17.7-14.3-32-32-32s-32 14.3-32 32v48H256V176c0-17.7-14.3-32-32-32s-32 14.3-32 32v48H128V176zM448 394.6c-8.5 3.3-18.2 5.4-28.8 5.4c-22.5 0-42.4-9.9-55.8-18.6c-4.1-2.7-7.8-5.4-10.9-7.8c-2.8 2.4-6.1 5-9.8 7.5C329.8 390 310.6 400 288 400s-41.8-10-54.6-18.9c-3.5-2.4-6.7-4.9-9.4-7.2c-2.7 2.3-5.9 4.7-9.4 7.2C201.8 390 182.6 400 160 400s-41.8-10-54.6-18.9c-3.7-2.6-7-5.2-9.8-7.5c-3.1 2.4-6.8 5.1-10.9 7.8C71.2 390.1 51.3 400 28.8 400c-10.6 0-20.3-2.2-28.8-5.4V480c0 17.7 14.3 32 32 32H416c17.7 0 32-14.3 32-32V394.6z"
+            />
+          </svg>
+          <h1>17</h1>
+          <p>Год на Рынке</p>
+          <button>ПОДРОБНЕЕ</button>
+        </div>
+
+        <div
+          class="card"
+          data-aos="flip-left"
+          data-aos-offset="250"
+          data-aos-duration="1500"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="46"
+            width="50"
+            viewBox="0 0 640 512"
+          >
+            <!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2023 Fonticons, Inc.-->
+            <path
+              fill="#03adfc"
+              d="M480 48c0-26.5-21.5-48-48-48H336c-26.5 0-48 21.5-48 48V96H224V24c0-13.3-10.7-24-24-24s-24 10.7-24 24V96H112V24c0-13.3-10.7-24-24-24S64 10.7 64 24V96H48C21.5 96 0 117.5 0 144v96V464c0 26.5 21.5 48 48 48H304h32 96H592c26.5 0 48-21.5 48-48V240c0-26.5-21.5-48-48-48H480V48zm96 320v32c0 8.8-7.2 16-16 16H528c-8.8 0-16-7.2-16-16V368c0-8.8 7.2-16 16-16h32c8.8 0 16 7.2 16 16zM240 416H208c-8.8 0-16-7.2-16-16V368c0-8.8 7.2-16 16-16h32c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16zM128 400c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16V368c0-8.8 7.2-16 16-16h32c8.8 0 16 7.2 16 16v32zM560 256c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H528c-8.8 0-16-7.2-16-16V272c0-8.8 7.2-16 16-16h32zM256 176v32c0 8.8-7.2 16-16 16H208c-8.8 0-16-7.2-16-16V176c0-8.8 7.2-16 16-16h32c8.8 0 16 7.2 16 16zM112 160c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16V176c0-8.8 7.2-16 16-16h32zM256 304c0 8.8-7.2 16-16 16H208c-8.8 0-16-7.2-16-16V272c0-8.8 7.2-16 16-16h32c8.8 0 16 7.2 16 16v32zM112 320H80c-8.8 0-16-7.2-16-16V272c0-8.8 7.2-16 16-16h32c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16zm304-48v32c0 8.8-7.2 16-16 16H368c-8.8 0-16-7.2-16-16V272c0-8.8 7.2-16 16-16h32c8.8 0 16 7.2 16 16zM400 64c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H368c-8.8 0-16-7.2-16-16V80c0-8.8 7.2-16 16-16h32zm16 112v32c0 8.8-7.2 16-16 16H368c-8.8 0-16-7.2-16-16V176c0-8.8 7.2-16 16-16h32c8.8 0 16 7.2 16 16z"
+            />
+          </svg>
+          <h1>21</h1>
+          <p>Заводов</p>
+          <button>ПОДРОБНЕЕ</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  cursor: pointer;
+}
+
+.about {
+  padding: 150px 0 80px;
+  background: #0e0e0e;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.about .container,
+.about-2 .container {
+  max-width: 1300px;
+  display: flex;
+  justify-content: center;
+  margin: 0 auto;
+}
+
+.about .container .left,
+.about .container .left {
+  width: 50%;
+  font-family: Cambria, Cochin, Georgia, Times, "Times New Roman", serif;
+  margin-left: 0;
+}
+
+.about .container .left img {
+  width: 350px;
+  box-shadow: 0 0 10px rgb(46, 46, 46);
+}
+
+.about .container .right {
+  margin-left: 100px;
+}
+
+.about .container .right .link-about {
+  display: flex;
+  gap: 20px;
+  margin-left: -55px;
+}
+
+.about .container .right .link-about h4 {
+  color: #03adfc;
+  width: 300px;
+  font-size: 25px;
+  margin: 0 0 30px;
+}
+
+.about .container .right h1 {
+  width: 500px;
+  font-size: 45px;
+  color: #fff;
+  margin-bottom: 40px;
+}
+
+.about .container .right p {
+  color: #757575;
+  font-size: 20px;
+  width: 500px;
+  border-left: 3px #03adfc solid;
+  padding-left: 30px;
+}
+
+.about-2 {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: #0e0e0e;
+  font-family: Cambria, Cochin, Georgia, Times, "Times New Roman", serif;
+  padding-bottom: 100px;
+}
+
+.about-2 .container .wrapper {
+  flex-wrap: wrap;
+  display: flex;
+  justify-content: space-between;
+  text-align: center;
+  gap: 35px;
+}
+
+.about-2 .container .wrapper .card {
+  width: 215px;
+  background: #252525;
+  padding: 15px 3px 10px;
+  border-radius: 5px;
+  margin: 0 auto;
+}
+
+.about-2 .container .wrapper .card h1 {
+  color: #03adfc;
+  font-size: 50px;
+}
+
+.about-2 .container .wrapper .card p {
+  color: #757575;
+  font-size: 17px;
+  line-height: 40px;
+}
+
+.about-2 .container .wrapper .card button {
+  display: block;
+  width: 90px;
+  height: 35px;
+  font-size: 10px;
+  color: #03adfc;
+  background: linear-gradient(to right, #03adfc 50%, #252525 50%);
+  border: 2px #03adfc solid;
+  margin: 0 auto;
+  background-size: 200% 100%;
+  background-position: right bottom;
+  transition: all 0.5s ease-out;
+}
+
+.about-2 .container .wrapper .card button:hover {
+  background-position: left bottom;
+  color: #fff;
+}
+
+/* Экраны с максимальной шириной 992px и менее */
+@media screen and (max-width: 992px) {
+  .about {
+    padding: 100px 30px 60px;
+  }
+
+  .about .container .left,
+  .about .container .right {
+    width: 100%;
+    margin-left: 0;
+  }
+
+  .about .container .right .link-about {
+    flex-direction: column;
+    margin-left: 0;
+    margin-bottom: 20px;
+  }
+
+  .about .container .right .link-about svg {
+    display: none;
+  }
+
+  .about .container .right .link-about h4 {
+    width: 100%;
+    margin-bottom: 20px;
+  }
+
+  .about .container .right h1,
+  .about .container .right p {
+    width: 100%;
+    border-left: none;
+    padding: 0;
+  }
+
+  .about-2 .container .wrapper {
+    justify-content: center;
+  }
+
+  .about-2 .container .wrapper .card {
+    width: 100%;
+  }
+}
+
+@media (max-width: 820px) {
+  .about .container,
+  .about-2 .container {
+    display: block;
+  }
+
+  .about .container .left img {
+    width: 100%;
+    height: 300px;
+  }
+  .about .container {
+    flex-direction: column;
+    text-align: center;
+  }
+
+  .about .container .right {
+    margin-left: 0;
+  }
+
+  .about-2 .container .wrapper .card {
+    width: 100%;
+    margin-bottom: 30px;
+  }
+}
+
+/* Экраны с максимальной шириной 576px и менее */
+@media screen and (max-width: 576px) {
+  .about .container {
+    padding: 0 0 40px;
+  }
+
+  .about .container .right .link-about {
+    flex-direction: column;
+    align-items: center;
+    margin-bottom: 20px;
+  }
+
+  .about .container .right .link-about h4 {
+    margin-bottom: 20px;
+  }
+
+  .about .container .right h1 {
+    font-size: 30px;
+  }
+}
+
+@media (max-width: 455px) {
+  .about .container .right h1 {
+    font-size: 25px;
+  }
+}
+</style>
